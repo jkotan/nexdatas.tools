d2cd423d92298e04dd5e9d840b080a502cb2c1a0
Jan Kotanski <jankotan@gmail.com>
2013-04-25 09:38:15 +0200

    updating setup.py

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#   This file is part of nexdatas - Tango Server for NeXus data writer
+#
+#    Copyright (C) 2012-2013 DESY, Jan Kotanski <jkotan@mail.desy.de>
+#
+#    nexdatas is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    nexdatas is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with nexdatas.  If not, see <http://www.gnu.org/licenses/>.
+## \package nexdatas.tools nexdatas
+## \file setup.py
+# GUI to create the XML components 
+
+import os, shutil, sys
+from distutils.core import setup
+from distutils.command.build import build
+from distutils.command.clean import clean
+
+
+## reading a file
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+
+## metadata for distutils
+SETUPDATA=dict(
+    name = "nexdatas.tools",
+    version = "1.0.0",
+    author = "Jan Kotanski, Eugen Wintersberger , Halil Pasic",
+    author_email = "jankotan@gmail.com, eugen.wintersberger@gmail.com, halil.pasic@gmail.com",
+    maintainer = "Jan Kotanski, Eugen Wintersberger , Halil Pasic",
+    maintainer_email = "jankotan@gmail.com, eugen.wintersberger@gmail.com, halil.pasic@gmail.com",
+    description = ("Configuration tool  for creating components"),
+    license = "GNU GENERAL PUBLIC LICENSE, version 3",
+    keywords = "configuration writer Tango component nexus data",
+    url = "http://code.google.com/p/nexdatas/",
+    platforms= ("Linux"),
+    packages=[ ],
+#    data_files = datas,
+    scripts = ['CMDtools/ndtscfg.py','CMDtools/ndtstdw.py'],
+#    package_data={'ndts': ['TDS']},
+#    long_description= read('README'),
+)
+
+## the main function
+def main():
+    setup(**SETUPDATA)
+        
+if __name__ == '__main__':
+    main()
