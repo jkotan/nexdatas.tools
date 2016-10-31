efd8e23439cb793621bd3baa6d7ab9d43cb4d24b
Jan Kotanski <jankotan@gmail.com>
2016-10-31 15:23:41 +0100

    first changes

--- nxstools/nxsconfig.py
+++ nxstools/nxsconfig.py
@@ -21,7 +21,7 @@
 
 import sys
 
-from optparse import OptionParser
+from argparse import ArgumentParser
 from .nxsparser import ParserTools, TableTools
 
 from .nxsdevicetools import (checkServer, listServers, openServer)
@@ -613,7 +613,33 @@ class ConfigServer(object):
                 ds, args, mandatory, private))
         return string
 
+def servers(args):
+    print "SERVERS"
 
+def _addargs(parser, private=True, args=None):
+    """ adds parser arguments"""
+
+    parser.add_argument("-s", "--server", dest="server",
+                      help="configuration server device name")
+    parser.add_argument("-d", "--datasources", action="store_true",
+                      default=False, dest="datasources",
+                      help="perform operation on datasources")
+    parser.add_argument("-m", "--mandatory", action="store_true",
+                      default=False, dest="mandatory",
+                      help="make use mandatory components")
+    if private:
+        parser.add_argument("-p", "--private", action="store_true",
+                            default=False, dest="private",
+                            help="make use private components,"
+                            " i.e. starting with '__'")
+    parser.add_argument("-n", "--no-newlines", action="store_true",
+                      default=False, dest="nonewlines",
+                      help="split result with space characters")
+
+    if args:
+        parser.add_argument('args', metavar='N', type=str, nargs=args,
+                            help='an integer for the accumulator')
+        
 def _createParser():
     """ creates command-line parameters parser
 
@@ -673,25 +699,55 @@ def _createParser():
             + "          show transformation parameters " \
             + "of given components \n"
 
-    #: (:class:`optparse.OptionParser`) option parser
-    parser = OptionParser(usage=usage)
-    parser.add_option("-s", "--server", dest="server",
-                      help="configuration server device name")
-    parser.add_option("-d", "--datasources", action="store_true",
-                      default=False, dest="datasources",
-                      help="perform operation on datasources")
-    parser.add_option("-m", "--mandatory", action="store_true",
-                      default=False, dest="mandatory",
-                      help="make use mandatory components")
-    parser.add_option("-p", "--private", action="store_true",
-                      default=False, dest="private",
-                      help="make use private components,"
-                      " i.e. starting with '__'")
-    parser.add_option("-n", "--no-newlines", action="store_true",
-                      default=False, dest="nonewlines",
-                      help="split result with space characters")
+    #: (:class:`argparse.ArgumentParser`) option parser
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers(help='sub-command help')
+    parser_list = subparsers.add_parser('list', help='list help')
+    _addargs(parser_list)
+    parser_show = subparsers.add_parser('show', help='show help')
+    _addargs(parser_show)
+    parser_get = subparsers.add_parser('get', help='get help')
+    _addargs(parser_get)
+    parser_variables = subparsers.add_parser('variables', help='variables help')
+    _addargs(parser_variables)
+    parser_sources = subparsers.add_parser('sources', help='sources help')
+    _addargs(parser_sources)
+    parser_servers = subparsers.add_parser('servers', help='servers help')
+    _addargs(parser_servers)
+    parser_record = subparsers.add_parser('record', help='record help')
+    _addargs(parser_record)
+    parser_merge = subparsers.add_parser('merge', help='merge help')
+    _addargs(parser_merge)
+    parser_components = subparsers.add_parser('components', help='components help')
+    _addargs(parser_components)
+    parser_data = subparsers.add_parser('data', help='data help')
+    _addargs(parser_data)
+    parser_describe = subparsers.add_parser('describe', help='describe help')
+    _addargs(parser_describe)
+    parser_info = subparsers.add_parser('info', help='info help')
+    _addargs(parser_info)
+    parser_geometry = subparsers.add_parser('geometry', help='geometry help')
+    _addargs(parser_geometry)
+    
+#    parser = ArgumentParser(usage=usage)
+#    parser.add_option("-s", "--server", dest="server",
+#                      help="configuration server device name")
+#    parser.add_option("-d", "--datasources", action="store_true",
+#                      default=False, dest="datasources",
+#                      help="perform operation on datasources")
+#    parser.add_option("-m", "--mandatory", action="store_true",
+#                      default=False, dest="mandatory",
+#                     help="make use mandatory components")
+#    parser.add_option("-p", "--private", action="store_true",
+#                      default=False, dest="private",
+#                      help="make use private components,"
+#                      " i.e. starting with '__'")
+#    parser.add_option("-n", "--no-newlines", action="store_true",
+#                      default=False, dest="nonewlines",
+#                      help="split result with space characters")
 
     return parser
+
 
 
 def main():
@@ -710,7 +766,9 @@ def main():
                 'describe': 0, 'info': 0, 'geometry': 0}
 
     parser = _createParser()
-    (options, args) = parser.parse_args()
+    options = parser.parse_args()
+    sys.exit(0)
+#    (options, args) = parser.parse_args()
 
     if args and args[0] == 'servers':
         print("\n".join(listServers(options.server, 'NXSConfigServer')))
