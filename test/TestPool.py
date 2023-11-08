50cd93736d90fc8aa45fef0799b40aee65724860
Jan Kotanski <jankotan@gmail.com>
2023-11-08 21:46:34 +0100

    Issue 523: update tests by changing tango.Device_4Impl to tango.LatestDeviceImpl (#524)
    * change tango.Device_4Impl to tango.LatestDeviceImpl
    
    * change tango.Device_4Impl to tango.LatestDeviceImpl
    
    * bump the version

--- test/TestPool.py
+++ test/TestPool.py
@@ -54,7 +54,7 @@ import json
 # =================================================================
 
 
-class Pool(tango.Device_4Impl):
+class Pool(tango.LatestDeviceImpl):
 
     # -------- Add you global variables here --------------------------
 
@@ -63,7 +63,7 @@ class Pool(tango.Device_4Impl):
     # -----------------------------------------------------------------
 
     def __init__(self, cl, name):
-        tango.Device_4Impl.__init__(self, cl, name)
+        tango.LatestDeviceImpl.__init__(self, cl, name)
 
         self.attr_value = ""
         self.attr_AcqChannelList = []
