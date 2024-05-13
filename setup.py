3ea147fc39ad97faf2d2cda0ef0ef8e0b51f687f
Jan Kotanski <jankotan@gmail.com>
2024-05-13 15:51:57 +0200

    add python 3.12

--- setup.py
+++ setup.py
@@ -125,6 +125,7 @@ SETUPDATA = dict(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     packages=["nxstools", "nxstools.xmltemplates", "nxstools.pyeval",
               "nxstools.ontology"],
